9ae8d1a0a25f4c0368fd82174f2897f7b1b692f1
hldgaoshuo <hldgaoshuo@163.com>
2025-12-10 18:49:57 +0800

    refactor(compiler): 重构编译器代码生成逻辑
    - 移除旧的代码生成相关字段和方法
    - 引入作用域(Scope)概念管理代码生成
    - 将代码生成操作委托给作用域对象
    - 更新所有操作码发射调用为作用域方法
    - 简化编译器结构体定义
    - 调整编译流程以适应新的作用域机制
    - 移除不再使用的导入包和辅助函数
    - 更新条件跳转和循环指令的偏移计算方式

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -1,27 +1,19 @@
 package compiler
 
 import (
-	"encoding/binary"
-	"os"
 	"stmt/ast"
 	object "stmt/object"
 	"stmt/token"
-
-	"google.golang.org/protobuf/proto"
 )
 
 type Compiler struct {
-	// in
-	ast []ast.Node
-	// out
-	code      []uint8
+	ast       []ast.Node
 	constants []*object.Object
 }
 
 func New(ast []ast.Node) *Compiler {
 	return &Compiler{
 		ast:       ast,
-		code:      []uint8{},
 		constants: []*object.Object{},
 	}
 }
@@ -34,13 +26,14 @@ func (c *Compiler) Compile() ([]uint8, []*object.Object, error) {
 			return nil, nil, err
 		}
 	}
+	mainScope := NewScope()
 	for _, node := range c.ast {
-		err := c.compile(node, symbolTable)
+		err := c.compile(node, symbolTable, mainScope)
 		if err != nil {
 			return nil, nil, err
 		}
 	}
-	return c.code, c.constants, nil
+	return mainScope.Code, c.constants, nil
 }
 
 func (c *Compiler) collectGlobal(node ast.Node, symbolTable *SymbolTable) error {
@@ -52,7 +45,7 @@ func (c *Compiler) collectGlobal(node ast.Node, symbolTable *SymbolTable) error 
 	}
 }
 
-func (c *Compiler) compile(node ast.Node, symbolTable *SymbolTable) error {
+func (c *Compiler) compile(node ast.Node, symbolTable *SymbolTable, scope *Scope) error {
 	switch _node := node.(type) {
 	case *ast.Literal:
 		switch value := _node.Value.(type) {
@@ -64,7 +57,7 @@ func (c *Compiler) compile(node ast.Node, symbolTable *SymbolTable) error {
 				ObjectType: object.ObjectType_OBJ_INT,
 			}
 			index := c.constantAdd(obj)
-			c.codeEmit(OP_CONSTANT, index)
+			scope.CodeEmit(OP_CONSTANT, index)
 			return nil
 		case float64:
 			obj := &object.Object{
@@ -74,17 +67,17 @@ func (c *Compiler) compile(node ast.Node, symbolTable *SymbolTable) error {
 				ObjectType: object.ObjectType_OBJ_FLOAT,
 			}
 			index := c.constantAdd(obj)
-			c.codeEmit(OP_CONSTANT, index)
+			scope.CodeEmit(OP_CONSTANT, index)
 			return nil
 		case bool:
 			if value {
-				c.codeEmit(OP_TRUE)
+				scope.CodeEmit(OP_TRUE)
 			} else {
-				c.codeEmit(OP_FALSE)
+				scope.CodeEmit(OP_FALSE)
 			}
 			return nil
 		case nil:
-			c.codeEmit(OP_NIL)
+			scope.CodeEmit(OP_NIL)
 			return nil
 		case string:
 			obj := &object.Object{
@@ -94,96 +87,96 @@ func (c *Compiler) compile(node ast.Node, symbolTable *SymbolTable) error {
 				ObjectType: object.ObjectType_OBJ_STRING,
 			}
 			index := c.constantAdd(obj)
-			c.codeEmit(OP_CONSTANT, index)
+			scope.CodeEmit(OP_CONSTANT, index)
 			return nil
 		default:
 			return ErrInvalidOperandType
 		}
 	case *ast.Grouping:
-		err := c.compile(_node.Expression, symbolTable)
+		err := c.compile(_node.Expression, symbolTable, scope)
 		if err != nil {
 			return err
 		}
 		return nil
 	case *ast.Unary:
-		err := c.compile(_node.Right, symbolTable)
+		err := c.compile(_node.Right, symbolTable, scope)
 		if err != nil {
 			return err
 		}
 		switch _node.Operator.TokenType {
 		case token.MINUS:
-			c.codeEmit(OP_NEGATE)
+			scope.CodeEmit(OP_NEGATE)
 			return nil
 		case token.BANG:
-			c.codeEmit(OP_NOT)
+			scope.CodeEmit(OP_NOT)
 			return nil
 		default:
 			return ErrInvalidOperatorType
 		}
 	case *ast.Binary:
-		err := c.compile(_node.Left, symbolTable)
+		err := c.compile(_node.Left, symbolTable, scope)
 		if err != nil {
 			return err
 		}
-		err = c.compile(_node.Right, symbolTable)
+		err = c.compile(_node.Right, symbolTable, scope)
 		if err != nil {
 			return err
 		}
 		switch _node.Operator.TokenType {
 		case token.PLUS:
-			c.codeEmit(OP_ADD)
+			scope.CodeEmit(OP_ADD)
 			return nil
 		case token.MINUS:
-			c.codeEmit(OP_SUBTRACT)
+			scope.CodeEmit(OP_SUBTRACT)
 			return nil
 		case token.STAR:
-			c.codeEmit(OP_MULTIPLY)
+			scope.CodeEmit(OP_MULTIPLY)
 			return nil
 		case token.SLASH:
-			c.codeEmit(OP_DIVIDE)
+			scope.CodeEmit(OP_DIVIDE)
 			return nil
 		case token.PERCENTAGE:
-			c.codeEmit(OP_MODULO)
+			scope.CodeEmit(OP_MODULO)
 			return nil
 		case token.GREATER:
-			c.codeEmit(OP_GT)
+			scope.CodeEmit(OP_GT)
 			return nil
 		case token.LESS:
-			c.codeEmit(OP_LT)
+			scope.CodeEmit(OP_LT)
 			return nil
 		case token.EQUAL_EQUAL:
-			c.codeEmit(OP_EQ)
+			scope.CodeEmit(OP_EQ)
 			return nil
 		case token.GREATER_EQUAL:
-			c.codeEmit(OP_GE)
+			scope.CodeEmit(OP_GE)
 			return nil
 		case token.LESS_EQUAL:
-			c.codeEmit(OP_LE)
+			scope.CodeEmit(OP_LE)
 			return nil
 		default:
 			return ErrInvalidOperatorType
 		}
 	case *ast.ExpressionStatement:
-		err := c.compile(_node.Expression, symbolTable)
+		err := c.compile(_node.Expression, symbolTable, scope)
 		if err != nil {
 			return err
 		}
 		if _, ok := _node.Expression.(*ast.Assign); !ok {
-			c.codeEmit(OP_POP)
+			scope.CodeEmit(OP_POP)
 		}
 		return nil
 	case *ast.Print:
-		err := c.compile(_node.Expression, symbolTable)
+		err := c.compile(_node.Expression, symbolTable, scope)
 		if err != nil {
 			return err
 		}
-		c.codeEmit(OP_PRINT)
+		scope.CodeEmit(OP_PRINT)
 		return nil
 	case *ast.Var:
 		if _node.Initializer == nil {
-			c.codeEmit(OP_NIL)
+			scope.CodeEmit(OP_NIL)
 		} else {
-			err := c.compile(_node.Initializer, symbolTable)
+			err := c.compile(_node.Initializer, symbolTable, scope)
 			if err != nil {
 				return err
 			}
@@ -194,10 +187,10 @@ func (c *Compiler) compile(node ast.Node, symbolTable *SymbolTable) error {
 		}
 		switch symbolInfo.Scope {
 		case LocalScope:
-			c.codeEmit(OP_SET_LOCAL, int(symbolInfo.Index))
+			scope.CodeEmit(OP_SET_LOCAL, int(symbolInfo.Index))
 			return nil
 		case GlobalScope:
-			c.codeEmit(OP_SET_GLOBAL, int(symbolInfo.Index))
+			scope.CodeEmit(OP_SET_GLOBAL, int(symbolInfo.Index))
 			return nil
 		default:
 			return ErrInvalidSymbolScope
@@ -209,16 +202,16 @@ func (c *Compiler) compile(node ast.Node, symbolTable *SymbolTable) error {
 		}
 		switch symbolInfo.Scope {
 		case LocalScope:
-			c.codeEmit(OP_GET_LOCAL, int(symbolInfo.Index))
+			scope.CodeEmit(OP_GET_LOCAL, int(symbolInfo.Index))
 			return nil
 		case GlobalScope:
-			c.codeEmit(OP_GET_GLOBAL, int(symbolInfo.Index))
+			scope.CodeEmit(OP_GET_GLOBAL, int(symbolInfo.Index))
 			return nil
 		default:
 			return ErrInvalidSymbolScope
 		}
 	case *ast.Assign:
-		err := c.compile(_node.Value, symbolTable)
+		err := c.compile(_node.Value, symbolTable, scope)
 		if err != nil {
 			return err
 		}
@@ -228,10 +221,10 @@ func (c *Compiler) compile(node ast.Node, symbolTable *SymbolTable) error {
 		}
 		switch symbolInfo.Scope {
 		case LocalScope:
-			c.codeEmit(OP_SET_LOCAL, int(symbolInfo.Index))
+			scope.CodeEmit(OP_SET_LOCAL, int(symbolInfo.Index))
 			return nil
 		case GlobalScope:
-			c.codeEmit(OP_SET_GLOBAL, int(symbolInfo.Index))
+			scope.CodeEmit(OP_SET_GLOBAL, int(symbolInfo.Index))
 			return nil
 		default:
 			return ErrInvalidSymbolScope
@@ -239,71 +232,71 @@ func (c *Compiler) compile(node ast.Node, symbolTable *SymbolTable) error {
 	case *ast.Block:
 		_symbolTable := NewSymbolTable(symbolTable)
 		for _, statement := range _node.Declarations {
-			err := c.compile(statement, _symbolTable)
+			err := c.compile(statement, _symbolTable, scope)
 			if err != nil {
 				return err
 			}
 		}
 		return nil
 	case *ast.If:
-		err := c.compile(_node.Condition, symbolTable)
+		err := c.compile(_node.Condition, symbolTable, scope)
 		if err != nil {
 			return err
 		}
-		offsetFalse := c.codeEmit(OP_JUMP_FALSE, 0)
-		c.codeEmit(OP_POP)
-		err = c.compile(_node.ThenBranch, symbolTable)
+		offsetFalse := scope.CodeEmit(OP_JUMP_FALSE, 0)
+		scope.CodeEmit(OP_POP)
+		err = c.compile(_node.ThenBranch, symbolTable, scope)
 		if err != nil {
 			return err
 		}
-		offset := c.codeEmit(OP_JUMP, 0)
-		err = c.codePatch(offsetFalse, OP_JUMP_FALSE)
+		offset := scope.CodeEmit(OP_JUMP, 0)
+		err = scope.CodePatch(offsetFalse, OP_JUMP_FALSE)
 		if err != nil {
 			return err
 		}
-		c.codeEmit(OP_POP)
+		scope.CodeEmit(OP_POP)
 		if _node.ElseBranch != nil {
-			err = c.compile(_node.ElseBranch, symbolTable)
+			err = c.compile(_node.ElseBranch, symbolTable, scope)
 			if err != nil {
 				return err
 			}
 		}
-		err = c.codePatch(offset, OP_JUMP)
+		err = scope.CodePatch(offset, OP_JUMP)
 		if err != nil {
 			return err
 		}
 		return nil
 	case *ast.Logical:
-		err := c.compile(_node.Left, symbolTable)
+		err := c.compile(_node.Left, symbolTable, scope)
 		if err != nil {
 			return err
 		}
 		switch _node.Operator.TokenType {
 		case token.AND:
-			offsetFalse := c.codeEmit(OP_JUMP_FALSE, 0)
-			c.codeEmit(OP_POP)
-			err = c.compile(_node.Right, symbolTable)
+			offsetFalse := scope.CodeEmit(OP_JUMP_FALSE, 0)
+			scope.CodeEmit(OP_POP)
+			err = c.compile(_node.Right, symbolTable, scope)
 			if err != nil {
 				return err
 			}
-			err = c.codePatch(offsetFalse, OP_JUMP_FALSE)
+			err = scope.CodePatch(offsetFalse, OP_JUMP_FALSE)
 			if err != nil {
 				return err
 			}
 			return nil
 		case token.OR:
-			offsetFalse := c.codeEmit(OP_JUMP_FALSE, 0)
-			offset := c.codeEmit(OP_JUMP, 0)
-			err = c.codePatch(offsetFalse, OP_JUMP_FALSE)
+			offsetFalse := scope.CodeEmit(OP_JUMP_FALSE, 0)
+			offset := scope.CodeEmit(OP_JUMP, 0)
+			err = scope.CodePatch(offsetFalse, OP_JUMP_FALSE)
 			if err != nil {
 				return err
 			}
-			c.codeEmit(OP_POP)
-			err = c.compile(_node.Right, symbolTable)
+			scope.CodeEmit(OP_POP)
+			err = c.compile(_node.Right, symbolTable, scope)
 			if err != nil {
 				return err
 			}
-			err = c.codePatch(offset, OP_JUMP)
+			err = scope.CodePatch(offset, OP_JUMP)
 			if err != nil {
 				return err
 			}
@@ -312,79 +305,27 @@ func (c *Compiler) compile(node ast.Node, symbolTable *SymbolTable) error {
 			return ErrInvalidOperatorType
 		}
 	case *ast.While:
-		loop := c.codeOffset()
-		err := c.compile(_node.Condition, symbolTable)
+		loop := scope.CodeOffset()
+		err := c.compile(_node.Condition, symbolTable, scope)
 		if err != nil {
 			return err
 		}
-		offsetFalse := c.codeEmit(OP_JUMP_FALSE, 0)
-		c.codeEmit(OP_POP)
-		err = c.compile(_node.Body, symbolTable)
+		offsetFalse := scope.CodeEmit(OP_JUMP_FALSE, 0)
+		scope.CodeEmit(OP_POP)
+		err = c.compile(_node.Body, symbolTable, scope)
 		if err != nil {
 			return err
 		}
-		c.codeEmit(OP_LOOP, loop)
-		err = c.codePatch(offsetFalse, OP_JUMP_FALSE)
+		scope.CodeEmit(OP_LOOP, loop)
+		err = scope.CodePatch(offsetFalse, OP_JUMP_FALSE)
 		if err != nil {
 			return err
 		}
-		c.codeEmit(OP_POP)
+		scope.CodeEmit(OP_POP)
 		return nil
 	default:
 		return ErrInvalidNodeType
 	}
-}
-
-// code
-func (c *Compiler) codePatch(offset int, op uint8) error {
-	_op := c.code[offset]
-	if _op != op {
-		return ErrOpCodeMismatch
-	}
-	operand := c.codeOffset()
-	code := c.codeMake(op, operand)
-	copy(c.code[offset:], code)
-	return nil
-}
-
-func (c *Compiler) codeEmit(op uint8, operands ...int) int {
-	offset := c.codeOffset()
-	code := c.codeMake(op, operands...)
-	c.code = append(c.code, code...)
-	return offset
-}
-
-func (c *Compiler) codeOffset() int {
-	offset := len(c.code)
-	return offset
-}
-
-func (c *Compiler) codeMake(op byte, operands ...int) []byte {
-	widths, ok := operandWidths[op]
-	if !ok {
-		return []byte{}
-	}
-
-	instructionLen := 1
-	for _, w := range widths {
-		instructionLen += w
-	}
-
-	instructions := make([]byte, instructionLen)
-	instructions[0] = op
-	offset := 1
-	for i, o := range operands {
-		width := widths[i]
-		switch width {
-		case 1:
-			instructions[offset] = byte(o)
-		case 2:
-			binary.BigEndian.PutUint16(instructions[offset:], uint16(o))
-		}
-		offset += width
-	}
-
-	return instructions
 }
 
 // constants
@@ -392,27 +333,4 @@ func (c *Compiler) constantAdd(obj *object.Object) int {
 	c.constants = append(c.constants, obj)
 	index := len(c.constants) - 1
 	return index
-}
-
-// chunk
-func (c *Compiler) chunk(name string) error {
-	chunk := &object.Chunk{
-		Code:         c.code,
-		Constants:    c.constants,
-		GlobalsCount: Global.NumDefinitions,
-	}
-
-	row, err := proto.Marshal(chunk)
-	if err != nil {
-		return err
-	}
-
-	path := "../vm2" + name + ".bin"
-
-	err = os.WriteFile(path, row, 0644)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
